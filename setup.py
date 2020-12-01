bb2e6d014c847d72424ebb5c3eafe19fce85606c
Morten Lied Johansen <mortenjo@ifi.uio.no>
2020-12-01 20:42:33 +0100

    Reorganise code to allow utility sharing across years

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+def read(filename):
+    f = open(filename)
+    contents = f.read()
+    f.close()
+    return contents
+
+
+setup(
+    name="advent_of_code",
+    version="1.0",
+    packages=["ibidem", "ibidem.advent_of_code"],
+    namespace_packages=["ibidem"],
+    zip_safe=True,
+    include_package_data=True,
+
+    # Requirements
+    install_requires=[
+        "numpy",
+        "vectormath",
+        "colorama",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-xdist",
+            "pytest-sugar",
+        ]
+    },
+
+    # Metadata
+    author="Morten Lied Johansen",
+    author_email="mortenjo@ifi.uio.no",
+    description="Advent of Code solutions",
+    long_description=read("README.rst"),
+    license="MIT",
+    keywords="ibidem advent_of_code",
+)
