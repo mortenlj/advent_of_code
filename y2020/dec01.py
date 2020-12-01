5c10a767b20d5b106245860f6faff4de782e4ed0
Morten Lied Johansen <mortenjo@ifi.uio.no>
2020-12-01 06:52:04 +0100

    2020-12-01

---
+++ y2020/dec01.py
@@ -0,0 +1,30 @@
+from util import get_input_name
+
+
+def load():
+    with open(get_input_name("dec01")) as fobj:
+        return [int(l) for l in fobj]
+
+
+def part1():
+    report = load()
+    for i in report:
+        for j in report:
+            if i + j == 2020:
+                print(i * j)
+                return
+
+
+def part2():
+    report = load()
+    for i in report:
+        for j in report:
+            for k in report:
+                if i + j + k == 2020:
+                    print(i * j * k)
+                    return
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
