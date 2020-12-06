d6b194153173ed17791b5956c0119351b8120270
Morten Lied Johansen <mortenjo@ifi.uio.no>
2020-12-06 20:34:55 +0100

    2020-12-06 - refactored

--- ibidem/advent_of_code/y2020/dec06.py
+++ ibidem/advent_of_code/y2020/dec06.py
@@ -1,52 +1,47 @@
 #!/usr/bin/env python
+import string
 
 from ibidem.advent_of_code.y2020.util import get_input_name
 
-ALL_ANSWERS = set("abcdefghijklmnopqrstuvwxyz")
+ALL_ANSWERS = set(string.ascii_lowercase)
 
 
-def load2():
+def load(update):
     with open(get_input_name("dec06")) as fobj:
         groups = []
         group = set()
         for line in fobj:
             if line.strip():
                 answers = set(line.strip())
-                nos = ALL_ANSWERS - answers
-                group.update(nos)
+                update(group, answers)
             else:
                 groups.append(group)
                 group = set()
         if group:
             groups.append(group)
         return groups
+
+
+def load2():
+    return load(lambda g, a: g.update(ALL_ANSWERS - a))
 
 
 def load1():
-    with open(get_input_name("dec06")) as fobj:
-        groups = []
-        group = set()
-        for line in fobj:
-            if line.strip():
-                group.update(line.strip())
-            else:
-                groups.append(group)
-                group = set()
-        if group:
-            groups.append(group)
-        return groups
+    return load(lambda g, a: g.update(a))
 
 
 def part1():
     groups = load1()
     result = sum(len(g) for g in groups)
     print(f"Result 1: {result}")
+    return result
 
 
 def part2():
     groups = load2()
     result = sum(len(ALL_ANSWERS - g) for g in groups)
     print(f"Result 2 {result}")
+    return result
 
 
 if __name__ == "__main__":
---
+++ ibidem/advent_of_code/y2020/test/test_dec06.py
@@ -0,0 +1,49 @@
+from unittest.mock import patch
+
+import pytest
+
+from ibidem.advent_of_code.y2020.dec06 import load1, load2, part1, part2
+
+GROUPS = """\
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+"""
+
+
+class TestDec06():
+    @pytest.fixture
+    def input(self, tmp_path):
+        input_file = tmp_path / "input.txt"
+        input_file.write_text(GROUPS)
+        with patch("ibidem.advent_of_code.y2020.dec06.get_input_name") as gin:
+            gin.return_value = input_file
+            yield gin
+
+    def test_load1(self, input):
+        groups = load1()
+        assert len(groups) == 5
+        assert [len(g) for g in groups] == [3, 3, 3, 1, 1]
+
+    def test_part1(self, input):
+        assert part1() == 11
+
+    def test_load2(self, input):
+        groups = load2()
+        assert len(groups) == 5
+        assert [len(g) for g in groups] == [26 - 3, 26 - 0, 26 - 1, 26 - 1, 26 - 1]
+
+    def test_part2(self, input):
+        assert part2() == 6
