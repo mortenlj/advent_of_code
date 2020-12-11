d316b8bd7fa36feec719eb0b730a1cec79cb2f1e
Morten Lied Johansen <mortenjo@ifi.uio.no>
2020-12-11 14:34:06 +0100

    2020-12-11 part2

--- ibidem/advent_of_code/y2020/dec11.py
+++ ibidem/advent_of_code/y2020/dec11.py
@@ -1,7 +1,63 @@
 #!/usr/bin/env python
 
+import numpy as np
+
 from ibidem.advent_of_code.board import Board
 from ibidem.advent_of_code.util import get_input_name
+
+
+class Part2Stepper():
+    def __init__(self, board):
+        self.board = board
+        self._directions = list(self._generate_directions())
+
+    def step(self):
+        b = self.board.copy()
+        changed = 0
+        for y in range(self.board.size_y):
+            for x in range(self.board.size_x):
+                v = self.board.get(x, y)
+                if v == ".":
+                    continue
+                count_occupied = self._count_occupied(x, y)
+                if v == "L":
+                    if count_occupied == 0:
+                        changed += 1
+                        b.set(x, y, "#")
+                elif v == "#":
+                    if count_occupied >= 5:
+                        changed += 1
+                        b.set(x, y, "L")
+        self.board = b
+        return changed
+
+    def _count_occupied(self, x, y):
+        count_occupied = 0
+        for dir in self._directions:
+            seat = self._look((x, y), dir)
+            if seat == "#":
+                count_occupied += 1
+        return count_occupied
+
+    def _generate_directions(self):
+        for j in (1, 0, -1):
+            for i in (1, 0, -1):
+                if i == j == 0:
+                    continue
+                yield i, j
+
+    def _look(self, pos, dir):
+        cur = np.array(pos)
+        dv = np.array(dir)
+        while True:
+            cur += dv
+            try:
+                v = self.board.get(cur[0], cur[1])
+                if v == ".":
+                    continue
+                return v
+            except IndexError:
+                return "."
 
 
 def load():
@@ -9,7 +65,7 @@ def load():
         return Board.from_string(fobj.read())
 
 
-def step(board):
+def step1(board):
     b = board.copy()
     for y in range(board.size_y):
         for x in range(board.size_x):
@@ -27,23 +83,35 @@ def step(board):
     return b
 
 
-def part1(current):
+def count(current, stepper):
     previous = None
     count = 0
     while current != previous:
         previous = current
-        current = step(current)
+        current = stepper(current)
         count += 1
     result = current.count("#")
-    print(f"After {count} steps, {result} seats are occupied")
     return result
 
 
-def part2():
-    pass
+def part1(board):
+    result = count(board, step1)
+    print(f"Part 1: After many steps, {result} seats are occupied")
+
+
+def part2(board):
+    stepper = Part2Stepper(board)
+    changed = 1
+    count = 0
+    while changed > 0:
+        changed = stepper.step()
+        count += 1
+    result = stepper.board.count("#")
+    print(f"Part 2: After {count} steps, {result} seats are occupied")
+    return result
 
 
 if __name__ == "__main__":
     board = load()
     part1(board)
-    part2()
+    part2(board)
--- ibidem/advent_of_code/y2020/test/test_dec11.py
+++ ibidem/advent_of_code/y2020/test/test_dec11.py
@@ -1,9 +1,12 @@
 import textwrap
 
+import pytest
+
 from ibidem.advent_of_code.y2020.dec11 import *
 
-TEST_BOARDS = (
-    textwrap.dedent("""\
+PART1_COUNT = 37
+PART2_COUNT = 26
+INITIAL_BOARD = Board.from_string(textwrap.dedent("""\
     L.LL.LL.LL
     LLLLLLL.LL
     L.L.L..L..
@@ -14,8 +17,8 @@ TEST_BOARDS = (
     LLLLLLLLLL
     L.LLLLLL.L
     L.LLLLL.LL
-    """),
-    textwrap.dedent("""\
+    """))
+PART1_STEP2_BOARD = Board.from_string(textwrap.dedent("""\
     #.##.##.##
     #######.##
     #.#.#..#..
@@ -26,44 +29,8 @@ TEST_BOARDS = (
     ##########
     #.######.#
     #.#####.##
-    """),
-    textwrap.dedent("""\
-    #.LL.L#.##
-    #LLLLLL.L#
-    L.L.L..L..
-    #LLL.LL.L#
-    #.LL.LL.LL
-    #.LLLL#.##
-    ..L.L.....
-    #LLLLLLLL#
-    #.LLLLLL.L
-    #.#LLLL.##
-    """),
-    textwrap.dedent("""\
-    #.##.L#.##
-    #L###LL.L#
-    L.#.#..#..
-    #L##.##.L#
-    #.##.LL.LL
-    #.###L#.##
-    ..#.#.....
-    #L######L#
-    #.LL###L.L
-    #.#L###.##
-    """),
-    textwrap.dedent("""\
-    #.#L.L#.##
-    #LLL#LL.L#
-    L.L.L..#..
-    #LLL.##.L#
-    #.LL.LL.LL
-    #.LL#L#.##
-    ..L.L.....
-    #L#LLLL#L#
-    #.LLLLLL.L
-    #.#L#L#.##
-    """),
-    textwrap.dedent("""\
+    """))
+PART1_FINAL_BOARD = Board.from_string(textwrap.dedent("""\
     #.#L.L#.##
     #LLL#LL.L#
     L.#.L..#..
@@ -74,19 +41,95 @@ TEST_BOARDS = (
     #L#L##L#L#
     #.LLLLLL.L
     #.#L#L#.##
-    """),
-)
+    """))
+PART2_STEP2_BOARD = Board.from_string(textwrap.dedent("""\
+    #.##.##.##
+    #######.##
+    #.#.#..#..
+    ####.##.##
+    #.##.##.##
+    #.#####.##
+    ..#.#.....
+    ##########
+    #.######.#
+    #.#####.##
+    """))
+PART2_STEP3_BOARD = Board.from_string(textwrap.dedent("""\
+    #.LL.LL.L#
+    #LLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLL#
+    #.LLLLLL.L
+    #.LLLLL.L#
+    """))
+PART2_FINAL_BOARD = Board.from_string(textwrap.dedent("""\
+    #.L#.L#.L#
+    #LLLLLL.LL
+    L.L.L..#..
+    ##L#.#L.L#
+    L.L#.LL.L#
+    #.LLLL#.LL
+    ..#.L.....
+    LLL###LLL#
+    #.LLLLL#.L
+    #.L#LL#.L#
+    """))
 
 
 class TestDec11():
     def test_step_one(self):
-        actual = step(Board.from_string(TEST_BOARDS[0]))
-        assert actual == Board.from_string(TEST_BOARDS[1])
+        actual = step1(INITIAL_BOARD)
+        assert actual == PART1_STEP2_BOARD
 
     def test_step_last(self):
-        actual = step(Board.from_string(TEST_BOARDS[-1]))
-        assert actual == Board.from_string(TEST_BOARDS[-1])
+        actual = step1(PART1_FINAL_BOARD)
+        assert actual == PART1_FINAL_BOARD
 
     def test_part1(self):
-        actual = part1(Board.from_string(TEST_BOARDS[0]))
-        assert actual == 37
+        actual = count(INITIAL_BOARD, step1)
+        assert actual == PART1_COUNT
+
+    def test_part2(self):
+        actual = part2(INITIAL_BOARD)
+        assert actual == PART2_COUNT
+
+
+class TestPart2Stepper():
+    def test_generate_directions(self):
+        stepper = Part2Stepper(None)
+        assert list(stepper._directions) == [(1, 1), (0, 1), (-1, 1),
+                                             (1, 0), (-1, 0),
+                                             (1, -1), (0, -1), (-1, -1)]
+
+    @pytest.mark.parametrize("pos, dv, expected", (
+            ((1, 1), np.array((-1, -1)), "L"),
+            ((1, 1), np.array((1, 1)), "."),
+            ((0, 0), np.array((1, 1)), "#"),
+            ((0, 0), np.array((0, 1)), "#"),
+            ((2, 0), np.array((0, -1)), "."),
+    ))
+    def test_look(self, pos, dv, expected):
+        stepper = Part2Stepper(Board.from_string("L.#\n"
+                                                 ".#.\n"
+                                                 "#L."))
+        actual = stepper._look(pos, dv)
+        assert actual == expected
+
+    @pytest.mark.parametrize("current, next", (
+            (INITIAL_BOARD, PART2_STEP2_BOARD),
+            (PART2_STEP2_BOARD, PART2_STEP3_BOARD),
+    ))
+    def test_step(self, current, next):
+        stepper = Part2Stepper(current)
+        stepper.step()
+        assert stepper.board == next
+
+    def test_step_last(self):
+        stepper = Part2Stepper(PART2_FINAL_BOARD)
+        changes = stepper.step()
+        assert changes == 0
+        assert stepper.board == PART2_FINAL_BOARD
