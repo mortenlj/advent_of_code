da15baa1b1e78a119bcaed3106b96e6120917c0c
Morten Lied Johansen <mortenjo@ifi.uio.no>
2020-12-14 15:51:58 +0100

    2020-12-14 part 2

--- ibidem/advent_of_code/y2020/dec14.py
+++ ibidem/advent_of_code/y2020/dec14.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
+import itertools
 import re
 
 import numpy as np
 
 from ibidem.advent_of_code.util import get_input_name
 
+MEM_PATTERN = re.compile(r"mem\[(\d+)\] = (\d+)")
 
-class Program():
-    MEM_PATTERN = re.compile(r"mem\[(\d+)\] = (\d+)")
 
+class Program1():
     def __init__(self):
         self.memory = {}
         self.where = None
@@ -17,9 +18,9 @@ class Program():
     def process(self, instructions):
         for inst in instructions:
             if inst.startswith("mask"):
-                self.where, self.mask_value = mask(inst[7:])
+                self.where, self.mask_value = self.mask(inst[7:])
             elif inst.startswith("mem"):
-                m = self.MEM_PATTERN.match(inst)
+                m = MEM_PATTERN.match(inst)
                 addr = int(m.group(1))
                 value = int(m.group(2))
                 self.set(addr, value)
@@ -29,8 +30,47 @@ class Program():
         result = np.where(self.where, self.mask_value, bin_value)
         self.memory[addr] = result
 
+    @staticmethod
+    def mask(m):
+        mask_value = np.array(list(m.replace("X", "0")), dtype=np.uint8)
+        where = np.array(list(m)) != "X"
+        return where, mask_value
+
     def calculate_sum(self):
         return sum(toint(v) for v in self.memory.values())
+
+
+class Program2():
+    def __init__(self):
+        self.memory = {}
+        self.floating_idx = None
+        self.fixed_idx = None
+
+    def process(self, instructions):
+        for inst in instructions:
+            if inst.startswith("mask"):
+                mask_values = np.array(list(inst[7:]))
+                self.floating_idx = np.where(mask_values == "X")[0]
+                self.fixed_idx = np.where(mask_values == "1")[0]
+            elif inst.startswith("mem"):
+                m = MEM_PATTERN.match(inst)
+                addr = int(m.group(1))
+                value = int(m.group(2))
+                self.set(addr, value)
+
+    def set(self, addr, value):
+        for mem in self.generate_addr(addr):
+            self.memory[mem] = value
+
+    def generate_addr(self, addr):
+        bin_addr = tobinarray(addr)
+        np.put(bin_addr, self.fixed_idx, 1)
+        for combo in itertools.product((0, 1), repeat=len(self.floating_idx)):
+            np.put(bin_addr, self.floating_idx, combo)
+            yield toint(bin_addr)
+
+    def calculate_sum(self):
+        return sum(self.memory.values())
 
 
 def load():
@@ -46,25 +86,23 @@ def toint(value):
     return sum(1 << i for i, b in enumerate(reversed(value)) if b)
 
 
-def mask(mask):
-    mask_value = np.array(list(mask.replace("X", "0")), dtype=np.uint8)
-    where = np.array(list(mask)) != "X"
-    return where, mask_value
-
-
 def part1(instructions):
-    program = Program()
+    program = Program1()
     program.process(instructions)
     result = program.calculate_sum()
-    print(f"The result of the initialisation process is {result}")
+    print(f"The result of the initialisation process v1 is {result}")
     return result
 
 
-def part2():
-    pass
+def part2(instructions):
+    program = Program2()
+    program.process(instructions)
+    result = program.calculate_sum()
+    print(f"The result of the initialisation process v2 is {result}")
+    return result
 
 
 if __name__ == "__main__":
     instructions = load()
     part1(instructions)
-    part2()
+    part2(instructions)
--- ibidem/advent_of_code/y2020/test/test_dec14.py
+++ ibidem/advent_of_code/y2020/test/test_dec14.py
@@ -1,3 +1,22 @@
+import pytest
+
+from ibidem.advent_of_code.y2020.dec14 import Program2
+
+PART2_PROGRAM = [
+    "mask = 000000000000000000000000000000X1001X",
+    "mem[42] = 100",
+    "mask = 00000000000000000000000000000000X0XX",
+    "mem[26] = 1",
+]
+PAR2_EXPECTED = 208
+
+
 class TestDec14():
-    def test_load(self):
-        pass
+    @pytest.mark.parametrize("instructions, expected", (
+            (PART2_PROGRAM, PAR2_EXPECTED),
+    ))
+    def test_program2(self, instructions, expected):
+        program = Program2()
+        program.process(instructions)
+        actual = program.calculate_sum()
+        assert actual == expected
