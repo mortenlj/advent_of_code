4a4318ce7c3c923b98dff524d6f91741eacba1ef
Morten Lied Johansen <mortenjo@ifi.uio.no>
2020-12-20 20:47:45 +0100

    2020-12-19 part1

---
+++ ibidem/advent_of_code/y2020/dec19.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import re
+
+from ibidem.advent_of_code.util import get_input_name
+
+
+class Rule:
+    NUM = re.compile(r"\d+")
+
+    def __init__(self, name, pattern):
+        self.name = name
+        self.pattern = pattern
+
+    def resolve(self, rules):
+        self.rules = rules
+        self.pattern, changes = self.NUM.subn(self.replace, self.pattern)
+        while changes > 0:
+            self.pattern, changes = self.NUM.subn(self.replace, self.pattern)
+        return self.pattern.replace(" ", "")
+
+    def replace(self, m):
+        rule = self.rules[m.group()]
+        return rule.resolve(self.rules)
+
+    def __repr__(self):
+        return f"Rule({self.name}, {self.pattern})"
+
+
+def parse_split_rule(line):
+    left, right = line.split("|")
+    left = parse_plain_rule(left)
+    right = parse_plain_rule(right)
+    return f"({left} | {right})"
+
+
+def parse_plain_rule(line):
+    parts = line.split()
+    if len(parts) == 1:
+        return line.strip()
+    return f"({line.strip()})"
+
+
+def load_rule(line, rules, _):
+    name, pattern = line.split(":")
+    pattern = pattern.strip()
+    if "|" in pattern:
+        pattern = parse_split_rule(pattern)
+    else:
+        pattern = parse_plain_rule(pattern)
+    rules[name] = Rule(name, pattern)
+
+
+def load_message(line, _, messages):
+    messages.append(line)
+
+
+def load():
+    rules = {}
+    messages = []
+    line_parser = load_rule
+    with open(get_input_name(19, 2020)) as fobj:
+        for line in fobj:
+            line = line.strip()
+            line = line.replace("\"", "")
+            if not line:
+                line_parser = load_message
+                continue
+            line_parser(line, rules, messages)
+    return rules, messages
+
+
+def part1(rules, messages):
+    start = rules["0"]
+    pattern = re.compile(start.resolve(rules))
+    count = 0
+    for message in messages:
+        if pattern.fullmatch(message):
+            count += 1
+    print(f"{count} messages matches rule 0")
+    return count
+
+
+def part2(rules, messages):
+    pass
+
+
+if __name__ == "__main__":
+    rules, messages = load()
+    part1(rules, messages)
+    part2(rules, messages)
---
+++ ibidem/advent_of_code/y2020/test/test_dec19.py
@@ -0,0 +1,61 @@
+from collections import namedtuple
+
+import pytest
+
+from ibidem.advent_of_code.y2020.dec19 import *
+
+TestCase = namedtuple("TestCase", ["rules", "messages", "matches"])
+
+CASE1 = TestCase(
+    {r.name: r for r in (Rule("0", "((1) (2))"), Rule("1", "a"), Rule("2", "((1 3) | (3 1))"), Rule("3", "b"))},
+    ["aab", "aba", "aabb", "baa"], ["aab", "aba"])
+CASE2 = TestCase({r.name: r for r in (Rule("0", "(4 1 5)"), Rule("1", "((2 3) | (3 2))"), Rule("2", "((4 4) | (5 5))"),
+                                      Rule("3", "((4 5) | (5 4))"), Rule("4", "a"), Rule("5", "b"))},
+                 ["ababbb", "bababa", "abbbab", "aaabbb", "aaaabbb"],
+                 ["ababbb", "abbbab"])
+CASE3 = TestCase({r.name: r for r in (Rule("0", "1 15"), Rule("1", "a"), Rule("15", "b"), Rule("5", "x"))},
+                 ["ab", "aab", "abb", "aabb"], ["ab"])
+
+
+class TestDec19():
+    @pytest.mark.parametrize("case", (
+            CASE1,
+            CASE2,
+            CASE3,
+    ))
+    def test_part1(self, case):
+        actual = part1(case.rules, case.messages)
+        assert actual == len(case.matches)
+
+    @pytest.mark.parametrize("input, pattern", (
+            ("0: 1", "1"),
+            ("0: 1 1", "(1 1)"),
+            ("0: 12", "12"),
+            ("0: 12 34", "(12 34)"),
+            ("0: 12 | 34", "(12 | 34)"),
+            ("0: 1 3 | 3 1", "((1 3) | (3 1))")
+    ))
+    def test_load_rule(self, input, pattern):
+        rules = {}
+        load_rule(input, rules, None)
+        assert rules["0"].pattern == pattern
+
+    @pytest.mark.parametrize("line, pattern", (
+            ("1", "1"),
+            ("1 1", "(1 1)"),
+            ("1 2 3", "(1 2 3)"),
+            ("12", "12"),
+            ("12 34", "(12 34)"),
+    ))
+    def test_parse_plain_rule(self, line, pattern):
+        actual = parse_plain_rule(line)
+        assert actual == pattern
+
+    @pytest.mark.parametrize("line, pattern", (
+            ("1 | 1", "(1 | 1)"),
+            ("1 2 | 3 4", "((1 2) | (3 4))"),
+            ("12 | 3 4", "(12 | (3 4))")
+    ))
+    def test_parse_split_rule(self, line, pattern):
+        actual = parse_split_rule(line)
+        assert actual == pattern
