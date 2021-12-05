24259a1c35199e84d1ea318510280f640f161b6a
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-12-05 13:45:03 +0100

    Day 5 - 2021

---
+++ ibidem/advent_of_code/y2021/dec05.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import re
+from collections import namedtuple
+
+import numpy as np
+
+from ibidem.advent_of_code.board import Board
+from ibidem.advent_of_code.util import get_input_name
+
+Pos = namedtuple("Pos", ("x", "y"))
+
+
+class Line:
+    _PATTERN = re.compile(r"(\d+),(\d+) -> (\d+),(\d+)")
+    start: Pos
+    end: Pos
+
+    def __init__(self, line):
+        m = self._PATTERN.match(line)
+        if not m:
+            raise RuntimeError("Invalid line: %r" % line)
+        self.start = Pos(int(m.group(1)), int(m.group(2)))
+        self.end = Pos(int(m.group(3)), int(m.group(4)))
+
+    def points(self):
+        y_start = min(self.start.y, self.end.y)
+        y_end = max(self.start.y, self.end.y)
+        x_start = min(self.start.x, self.end.x)
+        x_end = max(self.start.x, self.end.x)
+        for y in range(y_start, y_end+1):
+            for x in range(x_start, x_end+1):
+                yield Pos(x, y)
+
+    def __repr__(self):
+        return f"Line({self.start}, {self.end})"
+
+
+class DiagonalLine(Line):
+    def points(self):
+        x_step = 1 if self.start.x <= self.end.x else -1
+        y_step = 1 if self.start.y <= self.end.y else -1
+        cur = Pos(self.start.x, self.start.y)
+        yield cur
+        while cur.x != self.end.x and cur.y != self.end.y:
+            cur = Pos(cur.x+x_step, cur.y+y_step)
+            yield cur
+
+
+def load(with_diagonal):
+    with open(get_input_name(5, 2021)) as fobj:
+        return list(_load_input(fobj, with_diagonal))
+
+
+def _load_input(fobj, with_diagonal):
+    for s in fobj:
+        line = Line(s)
+        if line.start.x == line.end.x or line.start.y == line.end.y:
+            yield line
+        elif with_diagonal:
+            yield DiagonalLine(s)
+
+
+def part1(lines=None):
+    if lines is None:
+        lines = load(with_diagonal=False)
+    return solve(lines)
+
+
+def solve(lines):
+    board = Board(do_translate=False, fill_value=0, dtype=np.int_)
+    for line in lines:
+        for point in line.points():
+            cur = board.get(point.x, point.y)
+            board.set(point.x, point.y, cur + 1)
+    return np.sum(board.grid >= 2)
+
+
+def part2(lines=None):
+    if lines is None:
+        lines = load(with_diagonal=True)
+    return solve(lines)
+
+    
+if __name__ == "__main__":
+    p1_result = part1()
+    print(f"Part 1: {p1_result}")
+    p2_result = part2()
+    print(f"Part 2: {p2_result}")
---
+++ ibidem/advent_of_code/y2021/test/test_dec05.py
@@ -0,0 +1,102 @@
+import io
+
+import pytest
+
+from ibidem.advent_of_code.y2021.dec05 import *
+from ibidem.advent_of_code.y2021.dec05 import _load_input
+
+TEST_INPUT = io.StringIO("""\
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+""")
+
+PART1_RESULT = 5
+PART2_RESULT = 12
+
+
+class TestDec05():
+    @pytest.fixture
+    def input(self):
+        TEST_INPUT.seek(0)
+        return TEST_INPUT
+
+    @pytest.fixture
+    def without_diagonal_lines(self, input):
+        lines = _load_input(input, with_diagonal=False)
+        return lines
+
+    @pytest.fixture
+    def with_diagonal_lines(self, input):
+        lines = _load_input(input, with_diagonal=True)
+        return lines
+
+    def test_load_input_without_diagonal(self, input):
+        lines = list(_load_input(input, with_diagonal=False))
+        assert len(lines) == 6
+        assert lines[0].start == Pos(0, 9)
+        assert lines[0].end == Pos(5, 9)
+        assert lines[-1].start == Pos(3, 4)
+        assert lines[-1].end == Pos(1, 4)
+
+    def test_load_input_with_diagonal(self, input):
+        lines = list(_load_input(input, with_diagonal=True))
+        assert len(lines) == 10
+        assert lines[0].start == Pos(0, 9)
+        assert lines[0].end == Pos(5, 9)
+        assert lines[-1].start == Pos(5, 5)
+        assert lines[-1].end == Pos(8, 2)
+
+    def test_part1(self, without_diagonal_lines):
+        result = part1(list(without_diagonal_lines))
+        assert result == PART1_RESULT
+
+    def test_part2(self, with_diagonal_lines):
+        result = part1(list(with_diagonal_lines))
+        assert result == PART2_RESULT
+
+
+class TestLine:
+    def test_hor_line(self):
+        line = Line("0,0 -> 2,0")
+        actual_points = set(line.points())
+        assert actual_points == {Pos(0, 0), Pos(1, 0), Pos(2, 0)}
+
+    def test_ver_line(self):
+        line = Line("0,0 -> 0,2")
+        actual_points = set(line.points())
+        assert actual_points == {Pos(0, 0), Pos(0, 1), Pos(0, 2)}
+
+    def test_reversed_line(self):
+        line = Line("0,2 -> 0,0")
+        actual_points = set(line.points())
+        assert actual_points == {Pos(0, 2), Pos(0, 1), Pos(0, 0)}
+
+
+class TestDiagonalLine:
+    def test_down_right(self):
+        line = DiagonalLine("0,0 -> 2,2")
+        actual_points = set(line.points())
+        assert actual_points == {Pos(0, 0), Pos(1, 1), Pos(2, 2)}
+
+    def test_down_left(self):
+        line = DiagonalLine("2,0 -> 0,2")
+        actual_points = set(line.points())
+        assert actual_points == {Pos(0, 2), Pos(1, 1), Pos(2, 0)}
+
+    def test_up_right(self):
+        line = DiagonalLine("0,2 -> 2,0")
+        actual_points = set(line.points())
+        assert actual_points == {Pos(0, 2), Pos(1, 1), Pos(2, 0)}
+
+    def test_up_left(self):
+        line = DiagonalLine("2,2 -> 0,0")
+        actual_points = set(line.points())
+        assert actual_points == {Pos(0, 0), Pos(1, 1), Pos(2, 2)}
