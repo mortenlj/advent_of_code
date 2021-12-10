38a3c1f0568a6285da208304f267526031f4b2c0
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-12-10 10:19:12 +0100

    Day 10, part 2 - 2021

--- ibidem/advent_of_code/y2021/dec10.py
+++ ibidem/advent_of_code/y2021/dec10.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
 import enum
-from collections import defaultdict, deque
+from collections import deque
 
 from ibidem.advent_of_code.util import get_input_name
 
 
 class Pairs(enum.Enum):
-    NORMAL = ("(", ")", 3)
-    SQUARE = ("[", "]", 57)
-    CURLY = ("{", "}", 1197)
-    ANGLED = ("<", ">", 25137)
+    NORMAL = ("(", ")", 3, 1)
+    SQUARE = ("[", "]", 57, 2)
+    CURLY = ("{", "}", 1197, 3)
+    ANGLED = ("<", ">", 25137, 4)
 
-    def __init__(self, open, close, corruption_score):
+    def __init__(self, open, close, corruption_score, completion_score):
         self.characters = (open, close)
         self.open = open
         self.close = close
         self.corruption_score = corruption_score
+        self.completion_score = completion_score
 
     @classmethod
     def get(cls, c):
@@ -30,9 +31,9 @@ def load(fobj):
 
 
 def part1(input):
-    stack = deque()
     score = 0
     for line in input:
+        stack = deque()
         for c in line:
             cur = Pairs.get(c)
             if c == cur.close:
@@ -45,8 +46,30 @@ def part1(input):
     return score
 
 
+def completion_score(stack):
+    score = 0
+    for cur in reversed(stack):
+        score *= 5
+        score += cur.completion_score
+    return score
+
+
 def part2(input):
-    return None
+    scores = []
+    for line in input:
+        stack = deque()
+        for c in line:
+            cur = Pairs.get(c)
+            if c == cur.close:
+                other = stack.pop()
+                if not other == cur:
+                    stack = None
+                    break
+            if c == cur.open:
+                stack.append(cur)
+        if stack:
+            scores.append(completion_score(stack))
+    return sorted(scores)[len(scores) // 2]
 
 
 if __name__ == "__main__":
--- ibidem/advent_of_code/y2021/test/test_dec10.py
+++ ibidem/advent_of_code/y2021/test/test_dec10.py
@@ -1,8 +1,9 @@
 import io
+from collections import deque
+
 import pytest
 
-from ibidem.advent_of_code.y2021.dec10 import load, part1, part2
-
+from ibidem.advent_of_code.y2021.dec10 import load, part1, part2, completion_score, Pairs
 
 TEST_INPUT = io.StringIO("""\
 [({(<(())[]>[[{[]{<()<>>
@@ -33,11 +34,24 @@ class TestDec10():
 
     def test_load(self, loaded):
         assert len(loaded) == 10
-        
+
     def test_part1(self, loaded):
         result = part1(loaded)
         assert result == PART1_RESULT
-        
+
     def test_part2(self, loaded):
         result = part2(loaded)
         assert result == PART2_RESULT
+
+    @pytest.mark.parametrize("line, expected", (
+        ("}}]])})]", 288957),
+        (")}>]})", 5566),
+        ("}}>}>))))", 1480781),
+        ("]]}}]}]}>", 995444),
+        ("])}>", 294),
+    ))
+    def test_completion_score(self, line, expected):
+        my_line = (Pairs.get(c).open for c in reversed(line))
+        stack = deque(Pairs.get(c) for c in my_line)
+        actual = completion_score(stack)
+        assert actual == expected
