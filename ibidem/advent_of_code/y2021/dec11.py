d860b104f469bb7e3b18a5dc5ef4c7783272f15d
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-12-11 23:09:43 +0100

    Day 11, part 1 - 2021

---
+++ ibidem/advent_of_code/y2021/dec11.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import numpy as np
+
+from ibidem.advent_of_code.board import Board
+from ibidem.advent_of_code.util import get_input_name
+
+
+def load(fobj):
+    return Board.from_string(fobj.read(), fill_value=0, dtype=int, growable=False)
+
+
+def adjacent_view(grid, x, y):
+    x_min = max(x-1, 0)
+    y_min = max(y-1, 0)
+    x_max = min(x+2, grid.shape[1])
+    y_max = min(y+2, grid.shape[0])
+    return grid[(slice(y_min, y_max), slice(x_min, x_max))]
+
+
+def simulate_step(board):
+    board.grid += 1
+    work = np.ma.asarray(board.grid)
+    while (work > 9).any():
+        should_flash = np.transpose(np.nonzero(work > 9))
+        for y, x in should_flash:
+            to_flash = adjacent_view(work, x, y)
+            to_flash += 1
+            work[y, x] = np.ma.masked
+    board.grid[work.mask] = 0
+    return board.count(0)
+
+
+def part1(board):
+    total_flashes = 0
+    for step in range(100):
+        total_flashes += simulate_step(board)
+    return total_flashes
+
+
+def part2(board):
+    return None
+
+
+if __name__ == "__main__":
+    with open(get_input_name(11, 2021)) as fobj:
+        p1_result = part1(load(fobj))
+        print(f"Part 1: {p1_result}")
+    with open(get_input_name(11, 2021)) as fobj:
+        p2_result = part2(load(fobj))
+        print(f"Part 2: {p2_result}")
---
+++ ibidem/advent_of_code/y2021/test/test_dec11.py
@@ -0,0 +1,77 @@
+import io
+import pytest
+
+from ibidem.advent_of_code.y2021.dec11 import load, part1, part2, simulate_step
+
+TEST_INPUT = io.StringIO("""\
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
+""")
+
+STEP1 = load(io.StringIO("""\
+6594254334
+3856965822
+6375667284
+7252447257
+7468496589
+5278635756
+3287952832
+7993992245
+5957959665
+6394862637
+"""))
+
+STEP2 = load(io.StringIO("""\
+8807476555
+5089087054
+8597889608
+8485769600
+8700908800
+6600088989
+6800005943
+0000007456
+9000000876
+8700006848
+"""))
+
+PART1_RESULT = 1656
+PART2_RESULT = NotImplemented
+
+
+class TestDec11():
+    @pytest.fixture
+    def input(self):
+        TEST_INPUT.seek(0)
+        return TEST_INPUT
+
+    @pytest.fixture
+    def loaded(self, input):
+        return load(input)
+
+    def test_load(self, loaded):
+        assert loaded.size_x == 10
+        assert loaded.size_y == 10
+
+    def test_simulate_step(self, loaded):
+        flashes = simulate_step(loaded)
+        assert flashes == 0
+        assert loaded == STEP1
+        flashes = simulate_step(loaded)
+        assert flashes == 35
+        assert loaded == STEP2
+
+    def test_part1(self, loaded):
+        result = part1(loaded)
+        assert result == PART1_RESULT
+        
+    def test_part2(self, loaded):
+        result = part2(loaded)
+        assert result == PART2_RESULT
