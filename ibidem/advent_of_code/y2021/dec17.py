ff5b88d53b2d2d1e8c10b463e50d703c92e5e891
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-12-18 20:51:30 +0100

    Day 17, part 1 - 2021

---
+++ ibidem/advent_of_code/y2021/dec17.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import re
+from collections import namedtuple
+
+from vectormath import Vector2
+
+from ibidem.advent_of_code.util import get_input_name
+
+Target = namedtuple("Target", ("upper_left", "lower_right"))
+
+PATTERN = re.compile(r"target area: x=(\d+)\.\.(\d+), y=(-?\d+)\.\.(-?\d+)")
+
+
+def load(fobj):
+    s = fobj.read()
+    m = PATTERN.search(s)
+    upper_left = Vector2(int(m.group(1)), int(m.group(4)))
+    lower_right = Vector2(int(m.group(2)), int(m.group(3)))
+    return Target(upper_left, lower_right)
+
+
+class TooFar(Exception):
+    pass
+
+
+class TooShort(Exception):
+    pass
+
+
+def calculate_highest(vel, target):
+    pos = Vector2(0, 0)
+    heights = []
+    while pos.x <= target.lower_right.x and pos.y >= target.lower_right.y:
+        pos += vel
+        heights.append(pos.y)
+        vel.x = max(vel.x - 1, 0)
+        vel.y -= 1
+        if target.upper_left.x <= pos.x <= target.lower_right.x and target.lower_right.y <= pos.y <= target.upper_left.y:
+            return max(heights)
+        if vel.x == 0 and pos.x < target.upper_left.x:
+            raise TooShort
+    if pos.x > target.lower_right.x:
+        raise TooFar
+    if pos.x < target.upper_left.x:
+        raise TooShort
+    if pos.y < target.lower_right.y:
+        raise TooFar
+
+
+def part1(target):
+    max = 0
+    for x in range(1, int(target.lower_right.x)):
+        if ((x * (x + 1)) / 2) < target.upper_left.x:
+            continue
+        for y in range(1, abs(int(target.lower_right.y))):
+            try:
+                height = calculate_highest(Vector2(x, y), target)
+                if height > max:
+                    max = height
+            except TooFar:
+                pass  # break
+            except TooShort:
+                continue
+    return max
+
+
+def part2(input):
+    return None
+
+
+if __name__ == "__main__":
+    with open(get_input_name(17, 2021)) as fobj:
+        p1_result = part1(load(fobj))
+        print(f"Part 1: {p1_result}")
+    with open(get_input_name(17, 2021)) as fobj:
+        p2_result = part2(load(fobj))
+        print(f"Part 2: {p2_result}")
---
+++ ibidem/advent_of_code/y2021/test/test_dec17.py
@@ -0,0 +1,43 @@
+import io
+
+import pytest
+
+from ibidem.advent_of_code.y2021.dec17 import load, part1, part2
+
+TEST_INPUT = io.StringIO("""target area: x=20..30, y=-10..-5""")
+
+PART1_RESULT = 45
+PART2_RESULT = 112
+
+
+class TestDec17():
+    @pytest.fixture
+    def input(self):
+        TEST_INPUT.seek(0)
+        return TEST_INPUT
+
+    @pytest.fixture
+    def loaded(self, input):
+        return load(input)
+
+    def test_load(self, loaded):
+        assert loaded.upper_left.x == 20
+        assert loaded.upper_left.y == -5
+        assert loaded.lower_right.x == 30
+        assert loaded.lower_right.y == -10
+
+    def test_part1_testdata(self, loaded):
+        result = part1(loaded)
+        assert result == PART1_RESULT
+
+    @pytest.mark.parametrize("input, expected", (
+            (io.StringIO("target area: x=269..292, y=-68..-44"), 2278),
+    ))
+    def test_part1_realdata(self, input, expected):
+        loaded = load(input)
+        result = part1(loaded)
+        assert result == expected
+
+    def test_part2(self, loaded):
+        result = part2(loaded)
+        assert result == PART2_RESULT
