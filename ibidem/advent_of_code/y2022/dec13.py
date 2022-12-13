6eeedb82879b5de9c799f0958d4cf3370dd60cd0
Morten Lied Johansen <mortenjo@ifi.uio.no>
2022-12-13 19:50:33 +0100

    Day 13 - 2022

---
+++ ibidem/advent_of_code/y2022/dec13.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+from functools import cmp_to_key
+from itertools import chain
+
+from ibidem.advent_of_code.util import get_input_name
+
+DIVIDER_PACKAGES = (
+    [[2]],
+    [[6]],
+)
+
+
+def load(fobj):
+    pairs = []
+    current = []
+    for line in fobj:
+        if not line.strip():
+            pairs.append(tuple(current))
+            current = []
+            continue
+        value = eval(line)
+        current.append(value)
+    if current:
+        pairs.append(tuple(current))
+    return pairs
+
+
+def compare(pair):
+    match pair:
+        case [int(l), int(r)]:
+            return l - r
+        case [int(l), list(r)]:
+            return compare(([l], r))
+        case [list(l), int(r)]:
+            return compare((l, [r]))
+        case [list(l), list(r)]:
+            for pair in zip(l, r):
+                if (cmp := compare(pair)) != 0:
+                    return cmp
+            if len(l) != len(r):
+                return len(l) - len(r)
+    return 0
+
+
+def part1(pairs):
+    indexes = []
+    for i, pair in enumerate(pairs):
+        if compare(pair) < 0:
+            indexes.append(i + 1)
+    return sum(indexes)
+
+
+def part2(pairs):
+    packets = list(chain(DIVIDER_PACKAGES, *pairs))
+    packets = sorted(packets, key=cmp_to_key(lambda left, right: compare((left, right))))
+    indexes = []
+    for i, packet in enumerate(packets):
+        if packet in DIVIDER_PACKAGES:
+            indexes.append(i + 1)
+    return indexes[0] * indexes[1]
+
+
+if __name__ == "__main__":
+    with open(get_input_name(13, 2022)) as fobj:
+        p1_result = part1(load(fobj))
+        print(f"Part 1: {p1_result}")
+    with open(get_input_name(13, 2022)) as fobj:
+        p2_result = part2(load(fobj))
+        print(f"Part 2: {p2_result}")
---
+++ ibidem/advent_of_code/y2022/test/test_dec13.py
@@ -0,0 +1,74 @@
+import io
+
+import pytest
+
+from ibidem.advent_of_code.y2022.dec13 import load, part1, part2, compare
+
+TEST_INPUT = io.StringIO("""\
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+""")
+
+PART1_RESULT = 13
+PART2_RESULT = 140
+
+
+class TestDec13():
+    @pytest.fixture
+    def input(self):
+        TEST_INPUT.seek(0)
+        return TEST_INPUT
+
+    @pytest.fixture
+    def loaded(self, input):
+        return load(input)
+
+    def test_load(self, loaded):
+        assert len(loaded) == 8
+        assert all(len(pair) == 2 for pair in loaded)
+        assert loaded[0][0] == [1, 1, 3, 1, 1]
+
+    def test_part1(self, loaded):
+        result = part1(loaded)
+        assert result == PART1_RESULT
+
+    def test_part2(self, loaded):
+        result = part2(loaded)
+        assert result == PART2_RESULT
+
+    @pytest.mark.parametrize(("pair", "result"), (
+            ((1, 1), 0),
+            ((1, 2), -1),
+            ((2, 1), 1),
+            (([1], [1]), 0),
+            (([1], [2]), -1),
+            (([2], [1]), 1),
+            (([1], [1, 2]), -1),
+            ((1, [1]), 0),
+            ((1, [2]), -1),
+            ((2, [1]), 1),
+    ))
+    def test_compare(self, pair, result):
+        actual = compare(pair)
+        assert actual == result
