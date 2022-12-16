1088615b9dc2ad31d38f3402c7c7609e556d294e
Morten Lied Johansen <mortenjo@ifi.uio.no>
2022-12-16 19:19:08 +0100

    Day 15, part 2 - 2022

--- ibidem/advent_of_code/y2022/dec15.py
+++ ibidem/advent_of_code/y2022/dec15.py
@@ -2,6 +2,8 @@
 import re
 from collections import namedtuple
 
+from alive_progress import alive_it
+
 from ibidem.advent_of_code.util import get_input_name, time_this
 
 INPUT_PATTERN = re.compile(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
@@ -35,20 +37,48 @@ def part1(pairs, depth=2000000):
     for sensor, beacon in pairs:
         distance = (sensor - beacon).manhattan()
         if depth not in range(sensor.y - distance, sensor.y + distance):
-            # print(f"Sensor at {sensor} with closest beacon at {beacon} is out of range from depth {depth}, ignoring")
             continue
         rem = distance - abs(sensor.y - depth)
         for x in range(sensor.x - rem, sensor.x + rem + 1):
             line[(x, depth)] = 1
-        if sensor.y == depth:
-            line[(sensor.x, sensor.y)] = 1
         if beacon.y == depth:
             line[(beacon.x, beacon.y)] = 0
     return sum(line.values())
 
 
+def remove(possible_row, slice):
+    new_row = []
+    for element in possible_row:
+        if slice[1] < element[0]:
+            new_row.append(element)
+        elif slice[0] > element[1]:
+            new_row.append(element)
+        elif slice[0] > element[0] and slice[1] < element[1]:
+            new_row.append((element[0], slice[0] - 1))
+            new_row.append((slice[1] + 1, element[1]))
+        elif slice[0] <= element[0] and slice[1] >= element[1]:
+            pass
+        elif slice[0] <= element[0] <= slice[1]:
+            new_row.append((slice[1] + 1, element[1]))
+        elif slice[0] <= element[1] <= slice[1]:
+            new_row.append((element[0], slice[0] - 1))
+    return new_row
+
+
+@time_this
 def part2(pairs, max=4000000):
-    return None
+    for y in alive_it(range(max + 1)):
+        line = [(0, max)]
+        for sensor, beacon in pairs:
+            distance = (sensor - beacon).manhattan()
+            if (sensor.y - distance) < y <= (sensor.y + distance):
+                rem = distance - abs(sensor.y - y)
+                line = remove(line, (sensor.x - rem, sensor.x + rem))
+            if not line:
+                break
+        if line:
+            coord = line[0]
+            return coord[0] * TUNING_FREQ_MULTIPLIER + y
 
 
 if __name__ == "__main__":
--- ibidem/advent_of_code/y2022/test/test_dec15.py
+++ ibidem/advent_of_code/y2022/test/test_dec15.py
@@ -2,7 +2,7 @@ import io
 
 import pytest
 
-from ibidem.advent_of_code.y2022.dec15 import load, part1, part2
+from ibidem.advent_of_code.y2022.dec15 import load, part1, part2, remove
 
 TEST_INPUT = io.StringIO("""\
 Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -51,3 +51,46 @@ class TestDec15():
     def test_part2(self, loaded):
         result = part2(loaded, max=20)
         assert result == PART2_RESULT
+
+    @pytest.mark.parametrize("cut, expected", (
+            ((0, 4), [(5, 15)]),
+            ((0, 5), [(6, 15)]),
+            ((0, 6), [(7, 15)]),
+            ((14, 20), [(5, 13)]),
+            ((15, 20), [(5, 14)]),
+            ((16, 20), [(5, 15)]),
+            ((7, 10), [(5, 6), (11, 15)]),
+            ((4, 16), []),
+            ((4, 10), [(11, 15)]),
+            ((5, 10), [(11, 15)]),
+            ((6, 10), [(5, 5), (11, 15)]),
+            ((10, 16), [(5, 9)]),
+            ((10, 15), [(5, 9)]),
+            ((10, 14), [(5, 9), (15, 15)]),
+    ))
+    def test_remove_single(self, cut, expected):
+        line = [(5, 15)]
+        actual = remove(line, cut)
+        assert actual == expected
+
+    @pytest.mark.parametrize("cut, expected", (
+            ((0, 4), [(5, 10), (15, 20)]),
+            ((0, 5), [(6, 10), (15, 20)]),
+            ((0, 6), [(7, 10), (15, 20)]),
+            ((14, 20), [(5, 10)]),
+            ((15, 20), [(5, 10)]),
+            ((16, 20), [(5, 10), (15, 15)]),
+            ((7, 10), [(5, 6), (15, 20)]),
+            ((4, 16), [(17, 20)]),
+            ((4, 10), [(15, 20)]),
+            ((5, 10), [(15, 20)]),
+            ((6, 10), [(5, 5), (15, 20)]),
+            ((10, 16), [(5, 9), (17, 20)]),
+            ((10, 15), [(5, 9), (16, 20)]),
+            ((10, 14), [(5, 9), (15, 20)]),
+            ((0, 20), []),
+    ))
+    def test_remove_multi(self, cut, expected):
+        line = [(5, 10), (15, 20)]
+        actual = remove(line, cut)
+        assert actual == expected
