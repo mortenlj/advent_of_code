60fe0539b06a487cd73a5e451a25a87d9c05359f
Morten Lied Johansen <mortenjo@ifi.uio.no>
2022-12-21 14:35:52 +0100

    Empty placeholders for day 16, 17 and 19 - 2022

---
+++ ibidem/advent_of_code/y2022/test/test_dec17.py
@@ -0,0 +1,33 @@
+import io
+
+import pytest
+
+from ibidem.advent_of_code.y2022.dec17 import load, part1, part2
+
+TEST_INPUT = io.StringIO("""\
+""")
+
+PART1_RESULT = NotImplemented
+PART2_RESULT = NotImplemented
+
+
+class TestDec17():
+    @pytest.fixture
+    def input(self):
+        TEST_INPUT.seek(0)
+        return TEST_INPUT
+
+    @pytest.fixture
+    def loaded(self, input):
+        return load(input)
+
+    def test_load(self, loaded):
+        assert loaded
+
+    def test_part1(self, loaded):
+        result = part1(loaded)
+        assert result == PART1_RESULT
+
+    def test_part2(self, loaded):
+        result = part2(loaded)
+        assert result == PART2_RESULT
