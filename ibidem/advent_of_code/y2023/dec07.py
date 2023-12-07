8d4e84ec6c965aa37d60b9101d028d8a3bb28c67
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-12-07 12:26:46 +0100

    2023 - Day 7, part 2

--- ibidem/advent_of_code/y2023/dec07.py
+++ ibidem/advent_of_code/y2023/dec07.py
@@ -18,11 +18,11 @@ class HandType(IntEnum):
 
 @total_ordering
 class Hand:
-    def __init__(self, line):
+    def __init__(self, line, joker_mode):
         cards, bid = line.split()
         self.bid = int(bid.strip())
-        self.cards = [self._card_value(c) for c in cards.strip()]
-        self.type = self._hand_type(self.cards)
+        self.cards = [self._card_value(c, joker_mode) for c in cards.strip()]
+        self.type = self._hand_type(self.cards, joker_mode)
 
     def __eq__(self, other):
         return self.type == other.type and self.cards == other.cards
@@ -33,7 +33,9 @@ class Hand:
         return self.type < other.type
 
     @staticmethod
-    def _card_value(c):
+    def _card_value(c, joker_mode):
+        if joker_mode and c == "J":
+            return 1
         value_map = {
             "T": 10,
             "J": 11,
@@ -47,46 +49,76 @@ class Hand:
             return int(c)
 
     @staticmethod
-    def _hand_type(cards):
+    def _hand_type(cards, joker_mode):
         c = Counter(cards)
         current_type = HandType.HIGH_CARD
+        if joker_mode:
+            joker_count = c[1]
+            del c[1]
+        else:
+            joker_count = 0
+        if joker_count == 5:
+            return HandType.FIVE_OF_A_KIND
         for _, count in c.most_common():
             if count == 5:
                 return HandType.FIVE_OF_A_KIND
             if count == 4:
+                if joker_count == 1:
+                    return HandType.FIVE_OF_A_KIND
                 return HandType.FOUR_OF_A_KIND
             if count == 3:
+                if joker_count == 2:
+                    return HandType.FIVE_OF_A_KIND
+                if joker_count == 1:
+                    return HandType.FOUR_OF_A_KIND
                 current_type = HandType.THREE_OF_A_KIND
                 continue
             if count == 2:
+                if joker_count == 3:
+                    return HandType.FIVE_OF_A_KIND
+                if joker_count == 2:
+                    return HandType.FOUR_OF_A_KIND
+                if joker_count == 1:
+                    current_type = HandType.THREE_OF_A_KIND
+                    joker_count = 0
+                    continue
                 if current_type == HandType.THREE_OF_A_KIND:
                     return HandType.FULL_HOUSE
                 if current_type == HandType.ONE_PAIR:
                     return HandType.TWO_PAIRS
                 current_type = HandType.ONE_PAIR
                 continue
+            if count == 1:
+                if joker_count == 4:
+                    return HandType.FIVE_OF_A_KIND
+                if joker_count == 3:
+                    return HandType.FOUR_OF_A_KIND
+                if joker_count == 2:
+                    return HandType.THREE_OF_A_KIND
+                if joker_count == 1:
+                    if current_type == HandType.ONE_PAIR:
+                        return HandType.TWO_PAIRS
+                    current_type = HandType.ONE_PAIR
+                    joker_count = 0
+                    continue
         return current_type
 
 
-def load(fobj):
-    return [Hand(line) for line in fobj]
+def load(fobj, joker_mode):
+    return [Hand(line, joker_mode) for line in fobj]
 
 
-def part1(input):
+def solve(input):
     payouts = []
     for i, hand in enumerate(sorted(input)):
         payouts.append(hand.bid * (i + 1))
     return sum(payouts)
 
 
-def part2(input):
-    return None
-
-
 if __name__ == "__main__":
     with open(get_input_name(7, 2023)) as fobj:
-        p1_result = part1(load(fobj))
+        p1_result = solve(load(fobj, False))
         print(f"Part 1: {p1_result}")
     with open(get_input_name(7, 2023)) as fobj:
-        p2_result = part2(load(fobj))
+        p2_result = solve(load(fobj, True))
         print(f"Part 2: {p2_result}")
--- ibidem/advent_of_code/y2023/test/test_dec07.py
+++ ibidem/advent_of_code/y2023/test/test_dec07.py
@@ -1,7 +1,8 @@
 import io
+
 import pytest
 
-from ibidem.advent_of_code.y2023.dec07 import load, part1, part2, HandType
+from ibidem.advent_of_code.y2023.dec07 import load, solve, HandType, Hand
 
 TEST_INPUT = io.StringIO("""\
 32T3K 765
@@ -12,7 +13,7 @@ QQQJA 483
 """)
 
 PART1_RESULT = 6440
-PART2_RESULT = NotImplemented
+PART2_RESULT = 5905
 
 
 class TestDec07():
@@ -22,20 +23,51 @@ class TestDec07():
         return TEST_INPUT
 
     @pytest.fixture
-    def loaded(self, input):
-        return load(input)
+    def loaded1(self, input):
+        return load(input, False)
 
-    def test_load(self, loaded):
-        assert len(loaded) == 5
-        assert loaded[0].bid == 765
-        assert loaded[0].type == HandType.ONE_PAIR
-        assert loaded[0].cards == [3, 2, 10, 3, 13]
-        assert loaded[-1].type == HandType.THREE_OF_A_KIND
+    def test_load1(self, loaded1):
+        assert len(loaded1) == 5
+        assert loaded1[0].bid == 765
+        assert loaded1[0].type == HandType.ONE_PAIR
+        assert loaded1[0].cards == [3, 2, 10, 3, 13]
+        assert loaded1[-1].type == HandType.THREE_OF_A_KIND
 
-    def test_part1(self, loaded):
-        result = part1(loaded)
+    @pytest.fixture
+    def loaded2(self, input):
+        return load(input, True)
+
+    def test_load2(self, loaded2):
+        assert len(loaded2) == 5
+        assert loaded2[0].bid == 765
+        assert loaded2[0].type == HandType.ONE_PAIR
+        assert loaded2[0].cards == [3, 2, 10, 3, 13]
+        assert loaded2[1].type == HandType.FOUR_OF_A_KIND
+        assert loaded2[1].cards == [10, 5, 5, 1, 5]
+
+    def test_part1(self, loaded1):
+        result = solve(loaded1)
         assert result == PART1_RESULT
-        
-    def test_part2(self, loaded):
-        result = part2(loaded)
+
+    def test_part2(self, loaded2):
+        result = solve(loaded2)
         assert result == PART2_RESULT
+
+    @pytest.mark.parametrize("line,hand_type", [
+        ("JJJJJ 1", HandType.FIVE_OF_A_KIND),
+        ("TJJJJ 1", HandType.FIVE_OF_A_KIND),
+        ("KKJJJ 1", HandType.FIVE_OF_A_KIND),
+        ("KJKJK 1", HandType.FIVE_OF_A_KIND),
+        ("QQQQJ 1", HandType.FIVE_OF_A_KIND),
+        ("KQJJJ 1", HandType.FOUR_OF_A_KIND),
+        ("KQQJJ 1", HandType.FOUR_OF_A_KIND),
+        ("KQQQJ 1", HandType.FOUR_OF_A_KIND),
+        ("KQQQQ 1", HandType.FOUR_OF_A_KIND),
+        ("KQTJJ 1", HandType.THREE_OF_A_KIND),
+        ("KQTTJ 1", HandType.THREE_OF_A_KIND),
+        ("QQTTJ 1", HandType.FULL_HOUSE),
+        ("2345J 1", HandType.ONE_PAIR),
+    ])
+    def test_hand_type_joker(self, line, hand_type):
+        hand = Hand(line, True)
+        assert hand.type == hand_type
