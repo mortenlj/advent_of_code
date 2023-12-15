c097a14a8b5bafda70b297e6bbb61d121aeb4cfb
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-12-15 10:43:48 +0100

    2023 - Day 15, part 2

--- ibidem/advent_of_code/y2023/dec15.py
+++ ibidem/advent_of_code/y2023/dec15.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
+import re
+from collections import defaultdict
+from dataclasses import dataclass, field
 
 from ibidem.advent_of_code.util import get_input_name
+
+STEP_PATTERN = re.compile(r"([a-z]+)([=-])(\d+)?")
+
+
+@dataclass
+class Lens:
+    label: str
+    focal_length: int = field(compare=False)
 
 
 def load(fobj):
@@ -20,8 +31,36 @@ def part1(input):
     return sum(hash(step) for step in input)
 
 
+def focusing_power(box_num, slot, focal_length):
+    return box_num * slot * focal_length
+
+
 def part2(input):
-    return None
+    boxes = defaultdict(list)
+    for step in input:
+        if m := STEP_PATTERN.match(step):
+            label = m.group(1)
+            operation = m.group(2)
+            box = boxes[hash(label)]
+            if operation == "=":
+                focal_length = int(m.group(3))
+                lens = Lens(label, focal_length)
+                if lens in box:
+                    idx = box.index(lens)
+                    box[idx] = lens
+                else:
+                    box.append(lens)
+            else:
+                lens = Lens(label, 0)
+                if lens in box:
+                    box.remove(lens)
+    result = 0
+    for box_num, box in boxes.items():
+        if len(box) == 0:
+            continue
+        for slot, lens in enumerate(box):
+            result += focusing_power(box_num + 1, slot + 1, lens.focal_length)
+    return result
 
 
 if __name__ == "__main__":
--- ibidem/advent_of_code/y2023/test/test_dec15.py
+++ ibidem/advent_of_code/y2023/test/test_dec15.py
@@ -4,12 +4,12 @@ from collections import namedtuple
 
 import pytest
 
-from ibidem.advent_of_code.y2023.dec15 import load, part1, part2, hash
+from ibidem.advent_of_code.y2023.dec15 import load, part1, part2, hash, focusing_power
 
 TestData = namedtuple('TestData', 'part1 part2 input')
 
 TEST_INPUTS = [
-    TestData(1320, NotImplemented, io.StringIO(textwrap.dedent("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"))),
+    TestData(1320, 145, io.StringIO(textwrap.dedent("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"))),
 ]
 
 
@@ -51,3 +51,13 @@ class TestDec15():
     def test_hash(self, step, expected):
         actual = hash(step)
         assert actual == expected
+
+    @pytest.mark.parametrize("box_num,slot,focal_length,expected", [
+        (1, 1, 1, 1),
+        (1, 2, 2, 4),
+        (4, 1, 7, 28),
+        (4, 2, 5, 40),
+        (4, 3, 6, 72),
+    ])
+    def test_focusing_power(self, box_num, slot, focal_length, expected):
+        assert focusing_power(box_num, slot, focal_length) == expected
