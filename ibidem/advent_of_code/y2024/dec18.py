8b72d2c4870d1d5694bbc0b39d28ef96175f2283
Morten Lied Johansen <mortenjo@ifi.uio.no>
2024-12-20 22:45:06 +0100

    2024 - Day 18, part 2

--- ibidem/advent_of_code/y2024/dec18.py
+++ ibidem/advent_of_code/y2024/dec18.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import itertools
+
 from ibidem.advent_of_code.a_star import a_star, Node
 from ibidem.advent_of_code.board import Board
 from ibidem.advent_of_code.util import get_input_name, gen_list, Vector
@@ -18,8 +20,36 @@ def part1(positions, size, bytecount):
     return cost
 
 
-def part2(input):
-    return None
+def part2(positions, size, bytecount):
+    current_bc = bytecount
+    working_bytecount = 0
+    working_board = Board(size[0], size[1], do_translate=False, flip=False, fill_value=".", growable=False)
+    while current_bc > 0:
+        this_bytecount, working_board = find_last_working_bytecount(current_bc, positions[working_bytecount:], size, working_board)
+        working_bytecount += this_bytecount
+        print(f"Stepping {current_bc}: Last working board at bytecount {working_bytecount}")
+        if current_bc == 1:
+            break
+        current_bc = current_bc // 2
+    print("Last working board at bytecount", working_bytecount)
+    x, y = positions[working_bytecount]
+    return f"{x},{y}"
+
+
+def find_last_working_bytecount(bytecount, positions, size, clear_board):
+    working_board = clear_board.copy()
+    working_bytecount = 0
+    for i, bytes in enumerate(itertools.batched(positions, bytecount)):
+        board = working_board.copy()
+        for x, y in bytes:
+            board.set(x, y, "#")
+        _, path = a_star(Node(Vector(0, 0)), Vector(size[0] - 1, size[1] - 1), board)
+        if path is not None:
+            working_board = board
+            working_bytecount = (i + 1) * bytecount
+        else:
+            break
+    return working_bytecount, working_board
 
 
 if __name__ == "__main__":
@@ -27,5 +57,5 @@ if __name__ == "__main__":
         p1_result = part1(load(fobj), (71, 71), 1024)
         print(f"Part 1: {p1_result}")
     with open(get_input_name(18, 2024)) as fobj:
-        p2_result = part2(load(fobj))
+        p2_result = part2(load(fobj), (71, 71), 1024)
         print(f"Part 2: {p2_result}")
