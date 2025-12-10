8e56afbf8380b4ae88974c6aa5ca1b94f55b9dfa
Morten Lied Johansen <mortenjo@ifi.uio.no>
2025-12-10 12:09:03 +0100

    2025, day 9, part 2 for test input. Too slow for real input.

--- ibidem/advent_of_code/y2025/dec09.py
+++ ibidem/advent_of_code/y2025/dec09.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+import dataclasses
 import itertools
+from functools import lru_cache
 from heapq import heapify_max, heappush_max, heappop_max
 
 from rich.progress import track
@@ -28,8 +30,116 @@ def part1(red_tiles):
     return heappop_max(areas)
 
 
-def part2(input):
-    return None
+def inside(poly, v1, v2):
+    for x in range(min(v1.x, v2.x), max(v1.x, v2.x) + 1):
+        for y in range(min(v1.y, v2.y), max(v1.y, v2.y) + 1):
+            if Vector(x, y) not in poly:
+                return False
+    return True
+
+
+def orientation(p: Vector, q: Vector, r: Vector):
+    """find orientation of ordered triplet (p, q, r)
+    0 --> p, q and r are collinear
+    1 --> Clockwise
+    2 --> Counterclockwise
+    """
+    val = (q.y - p.y) * (r.x - q.x) - \
+          (q.x - p.x) * (r.y - q.y)
+
+    # collinear
+    if val == 0:
+        return 0
+
+    # clock or counterclock wise
+    # 1 for clockwise, 2 for counterclockwise
+    return 1 if val > 0 else 2
+
+
+@dataclasses.dataclass(frozen=True)
+class Line:
+    start: Vector
+    end: Vector
+
+    def __contains__(self, point):
+        return (max(self.start.x, self.end.x) >= point.x >= min(self.start.x, self.end.x) and
+                max(self.start.y, self.end.y) >= point.y >= min(self.start.y, self.end.y))
+
+    def intersects(self, other: Line):
+        # find the four orientations needed
+        # for general and special cases
+        o1 = orientation(self.start, self.end, other.start)
+        o2 = orientation(self.start, self.end, other.end)
+        o3 = orientation(other.start, other.end, self.start)
+        o4 = orientation(other.start, other.end, self.end)
+
+        # general case
+        if o1 != o2 and o3 != o4:
+            return True
+
+        # special cases
+        # p1, q1 and p2 are collinear and p2 lies on segment p1q1
+        if o1 == 0 and other.start in self:
+            return True
+
+        # p1, q1 and q2 are collinear and q2 lies on segment p1q1
+        if o2 == 0 and other.end in self:
+            return True
+
+        # p2, q2 and p1 are collinear and p1 lies on segment p2q2
+        if o3 == 0 and self.start in other:
+            return True
+
+        # p2, q2 and q1 are collinear and q1 lies on segment p2q2
+        if o4 == 0 and self.end in other:
+            return True
+
+        return False
+
+    def points(self):
+        for x in range(self.start.x, self.end.x + 1):
+            for y in range(self.start.y, self.end.y + 1):
+                yield Vector(x, y)
+
+
+class Polygon:
+    def __init__(self, lines):
+        self.lines = lines
+
+    @lru_cache(maxsize=None)
+    def __contains__(self, item):
+        exit_line = Line(item, Vector(item.x, 0))
+        count = 0
+        for line in self.lines:
+            if item == line.start or item == line.end:
+                return True
+            if item in line:
+                return True
+            if line.intersects(exit_line):
+                count += 1
+        return count % 2 != 0
+
+    def __repr__(self):
+        return f"Polygon(lines={self.lines})"
+
+
+def make_poly(red_tiles):
+    poly = []
+    tiles = red_tiles + [red_tiles[0]]
+    for v1, v2 in itertools.pairwise(tiles):
+        assert v1.x == v2.x or v1.y == v2.y
+        poly.append(Line(v1, v2))
+    return Polygon(poly)
+
+
+def part2(red_tiles):
+    poly = make_poly(red_tiles)
+    largest = 0
+    for v1, v2 in track(itertools.combinations(red_tiles, 2)):
+        area = (abs(v1.x - v2.x) + 1) * (abs(v1.y - v2.y) + 1)
+        if area > largest and inside(poly, v1, v2):
+            largest = area
+    return largest
 
 
 if __name__ == "__main__":
--- ibidem/advent_of_code/y2025/test/test_dec09.py
+++ ibidem/advent_of_code/y2025/test/test_dec09.py
@@ -5,12 +5,12 @@ from collections import namedtuple
 import pytest
 
 from ibidem.advent_of_code.util import Vector
-from ibidem.advent_of_code.y2025.dec09 import load, part1, part2
+from ibidem.advent_of_code.y2025.dec09 import load, part1, part2, make_poly, inside, Polygon, Line
 
 Case = namedtuple('Case', 'part1 part2 input')
 
 TEST_INPUTS = [
-    Case(50, NotImplemented, io.StringIO(textwrap.dedent("""\
+    Case(50, 24, io.StringIO(textwrap.dedent("""\
         7,1
         11,1
         11,7
@@ -36,11 +36,52 @@ class TestDec09():
     def test_load(self, loaded):
         assert len(loaded) == 8
         assert isinstance(loaded[0], Vector)
-        
+
+    @pytest.fixture
+    def poly(self, loaded):
+        return make_poly(loaded)
+
+    @pytest.mark.parametrize("p, expected", (
+        (Vector(1, 1), True),
+        (Vector(1, 2), True),
+        (Vector(1, 3), True),
+        (Vector(2, 1), True),
+        (Vector(2, 2), True),
+        (Vector(2, 3), True),
+        (Vector(3, 1), True),
+        (Vector(3, 2), True),
+        (Vector(3, 3), True),
+        (Vector(7, 3), False),
+        (Vector(9, 7), False),
+        (Vector(9, 5), False),
+    ))
+    def test_poly_contains(self, p, expected):
+        top_left = Vector(1, 1)
+        top_right = Vector(3, 1)
+        bottom_left = Vector(1, 3)
+        bottom_right = Vector(3, 3)
+        lines = [
+            Line(top_left, top_right),
+            Line(top_right, bottom_right),
+            Line(bottom_right, bottom_left),
+            Line(bottom_left, top_left)
+        ]
+        poly = Polygon(lines)
+        assert (p in poly) == expected
+
+    @pytest.mark.parametrize("v1, v2, expected", (
+            (Vector(1, 1), Vector(2, 2), False),
+            (Vector(7, 3), Vector(11, 1), True),
+            (Vector(9, 7), Vector(9, 5), True),
+            (Vector(9, 5), Vector(2, 3), True),
+    ))
+    def test_inside(self, poly, v1, v2, expected):
+        assert inside(poly, v1, v2) == expected
+
     def test_part1(self, loaded, case):
         result = part1(loaded)
         assert result == case.part1
-        
+
     def test_part2(self, loaded, case):
         result = part2(loaded)
         assert result == case.part2
